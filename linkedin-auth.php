80fed3ec6c3f64388cfcebf1eb9748f42de13d77
Sufi Nawaz <sufi@eaiti.com>
2014-11-10 13:38:08 -0500

    cleanup

--- linkedin-auth.php
+++ linkedin-auth.php
@@ -1,105 +1,67 @@
 <?php
-define('API_KEY',      get_option( 'sufi_linkedin_opts' )["key"]);
-define('API_SECRET',   get_option( 'sufi_linkedin_opts' )["secret"]);
-define('REDIRECT_URI', 'http://localhost/wp/?page_id=6');
-define('SCOPE',        'r_basicprofile r_emailaddress rw_groups');
- 
-// You'll probably use a database
-session_name('linkedin');
+define( 'API_KEY',      get_option( 'sufi_linkedin_opts' )["key"] );
+define( 'API_SECRET',   get_option( 'sufi_linkedin_opts' )["secret"] );
+define( 'REDIRECT_URI', get_option( 'sufi_linkedin_opts' )["uri"] );
+define( 'SCOPE',        'r_emailaddress rw_groups' );
+
+session_name( 'linkedin' );
 session_start();
- 
+
 function getAuthorizationCode() {
     $params = array(
         'response_type' => 'code',
         'client_id' => get_option( 'sufi_linkedin_opts' )["key"],
         'scope' => SCOPE,
-        'state' => uniqid('', true), // unique long string
-        'redirect_uri' => REDIRECT_URI,
+        'state' => uniqid( '', true ),
+        'redirect_uri' => REDIRECT_URI
     );
- 
-    // Authentication request
-    $url = 'https://www.linkedin.com/uas/oauth2/authorization?' . http_build_query($params);
-     
-    // Needed to identify request when it returns to us
+    $url = 'https://www.linkedin.com/uas/oauth2/authorization?' . http_build_query( $params );
     $_SESSION['state'] = $params['state'];
- 
-    // Redirect user to authenticate
-    // header("Location: $url");
-    // echo "<br />";
     return $url;
 }
-     
+
 function getAccessToken() {
+    // Request the Access Token
     $params = array(
         'grant_type' => 'authorization_code',
         'client_id' => API_KEY,
         'client_secret' => API_SECRET,
-        'code' => $_GET['code'],
         'redirect_uri' => REDIRECT_URI,
+        'code' => $_GET['code']
     );
-     
-    // Access Token request
-    $url = 'https://www.linkedin.com/uas/oauth2/accessToken?'. http_build_query($params);
-     
-    // Tell streams to make a POST request
+    $url = 'https://www.linkedin.com/uas/oauth2/accessToken?'. http_build_query( $params );
     $context = stream_context_create(
-        array('http' => 
-            array('method' => 'POST',
+        array( 'http' =>
+            array( 'method' => 'POST',
             )
         )
     );
- 
-    // Retrieve access token information
-    // print  "entered access token";
-    $response = @file_get_contents($url, false, $context);
-
-    // var_dump( $response);
- 
-    // Native PHP object, please
-    $token = json_decode($response);
- 
-    // Store access token and expiration time
-    // echo "<pre>";
-    // var_dump($token);
-    // echo "</pre>";
-    if(isset($token)){
-    $_SESSION['access_token'] = $token->access_token; // guard this! 
-    // echo $_SESSION['access_token'];
-    $_SESSION['expires_in']   = $token->expires_in; // relative time (in seconds)
-    $_SESSION['expires_at']   = time() + $_SESSION['expires_in']; // absolute time
-     
-    return true;
+    $response = file_get_contents( $url, false, $context );
+    $token = json_decode( $response );
+    if ( isset( $token ) ) {
+        $_SESSION['access_token'] = $token->access_token;
+        $_SESSION['expires_in']   = $token->expires_in;
+        $_SESSION['expires_at']   = time() + $_SESSION['expires_in'];
+        return true;
     } else {
         return false;
     }
 }
- 
-function fetch($method, $resource, $body = '') {
-    // $_SESSION['access_token']="";
-    // print $_SESSION['access_token'];
+
+function fetch( $method, $resource, $body = '' ) {
     $opts = array(
         'http'=>array(
             'method' => $method,
             'header' => "Authorization: Bearer " . $_SESSION['access_token'] . "\r\n" . "x-li-format: json\r\n"
         )
     );
- 
-    // Need to use HTTPS
     $url = 'https://api.linkedin.com' . $resource;
- 
-    // Append query parameters (if there are any)
-    if (isset($params) && count($params)) { $url .= '?' . http_build_query($params); }
- 
-    // Tell streams to make a (GET, POST, PUT, or DELETE) request
-    // And use OAuth 2 access token as Authorization
-    $context = stream_context_create($opts);
- 
-    // Hocus Pocus
+    if ( isset( $params ) && count( $params ) ) { $url .= '?' . http_build_query( $params ); }
+    $context = stream_context_create( $opts );
     try{
-        $response = @file_get_contents($url, false, $context);
-        // Native PHP object, please
-        return json_decode($response);
-    } catch (Exception $e) {
+        $response = @file_get_contents( $url, false, $context );
+        return json_decode( $response );
+    } catch ( Exception $e ) {
         return null;
     }
 }
--- linkedin-groups-block.php
+++ linkedin-groups-block.php
@@ -1,85 +1,79 @@
 <?php
 /**
+ *
  * @package LinkedIn Group Membership Conditional Blocks
  * @version 0.1.0
  */
 /*
 Plugin Name: LinkedIn Group Membership Conditional Blocks
 Plugin URI: http://sufinawaz.com/plugins/linkedin-groups-block
-Description: With this plugin one can use short codes to hide/show a block of codes depending on whether or not a user belongs to a LinkedIn group. This requires users to authenticate using LinkedIn. 
+Description: With this plugin one can use short codes to hide/show a block of codes depending on whether or not a user belongs to a LinkedIn group. This requires users to authenticate using LinkedIn.
 Author: Sufi Nawaz
 Version: 0.1.0
 Author URI: http://sufinawaz.com/
 */
 
-defined('ABSPATH') or die("No script kiddies please!");
+defined( 'ABSPATH' ) or die( "No script kiddies please!" );
 
 foreach ( glob( plugin_dir_path( __FILE__ ) . "/*.php" ) as $file ) {
-    include_once $file;
+	include_once $file;
 }
 
 /*****************************************************
  *  Options Menu       								 *
  *****************************************************/
 
-/** Step 1. */
 function linkedin_api_menu() {
 	add_options_page( 'LinkedIn', 'LinkedIn Groups', 'manage_options', 'linkedin-groups-block', 'linkedin_api_options' );
 }
 
-/** Step 2. */
 add_action( 'admin_menu', 'linkedin_api_menu' );
 
-/** Step 3. */
 function linkedin_api_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
+	if ( !current_user_can( 'manage_options' ) ) {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
-	// variables for the field and option names 
+	// variables for the field and option names
 	$hidden_field_name = 'mt_submit_hidden';
 	$key_field_name = 'linkedin_api_key';
-	$secret_field_name = 'linkedin_api_secret';
 	$uri_field_name = 'linkedin_api_redirect_uri';
+	$secret_field_name = 'linkedin_api_secret';
 
-    // Read in existing option value from database
-    $optsArray = get_option( 'sufi_linkedin_opts' );
-    $key_val = $optsArray["key"];
-    $secret_val = $optsArray["secret"];
-    $uri_val = $optsArray["uri"];
+	// Read in existing option value from database
+	$getOpts = get_option( 'sufi_linkedin_opts' );
+	$key_val = $getOpts["key"];
+	$uri_val = $getOpts["uri"];
+	$secret_val = $getOpts["secret"];
 
-    // See if the user has posted us some information
-    // If they did, this hidden field will be set to 'Y'
-    if( isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y' ) {
-        // Read their posted value
-        $key_val = $_POST[ $key_field_name ];
-        $secret_val = $_POST[ $secret_field_name ];
-        $uri_val = $_POST[ $uri_field_name ];
+	if ( isset( $_POST[ $hidden_field_name ] ) && $_POST[ $hidden_field_name ] == 'Y' ) {
+		$key_val = $_POST[ $key_field_name ];
+		$uri_val = $_POST[ $uri_field_name ];
+		$secret_val = $_POST[ $secret_field_name ];
 
-        $opts = array(
-        	"key"=>$key_val,
-        	"secret"=>$secret_val,
-        	"uri"=>$uri_val
-        	);
-        // Save the posted value in the database
-        update_option( 'sufi_linkedin_opts', $opts );
-        // Put an settings updated message on the screen
-        echo '<div class="updated"><p><strong>Settings saved.</strong></p></div>';
-    }
-    echo '<div class="wrap">';
-    echo "<h2>" . __( 'LinkedIn API Settings', 'linkedin-groups-block' ) . "</h2>";
+		$updateOpts = array(
+			"key"=>$key_val,
+			"uri"=>$uri_val,
+			"secret"=>$secret_val
+		);
+		// Save the posted value in the database
+		update_option( 'sufi_linkedin_opts', $updateOpts );
+		echo '<div class="updated"><p><strong>Settings saved.</strong></p></div>';
+	}
+	echo '<div class="wrap">';
+	echo "<h2>" . __( 'LinkedIn API Settings', 'linkedin-groups-block' ) . "</h2>";
 
 	// settings form
 	echo <<<HEREDOC
 		<p>If you do not have an API key and secret, please click <a href="https://www.linkedin.com/secure/developer?newapp=" target="_blank" title="Create new LinkedIn Application">here</a> to create a new LinkedIn Application.</p>
-		<form name="linkedin-api-form" method="post" action="">
-		<input type="hidden" name="$hidden_field_name" value="Y">
-		<p>API Key: <input type="text" name="$key_field_name" value="$key_val" size="20"></p>
-		<p>API Secret: <input type="text" name="$secret_field_name" value="$secret_val" size="20"></p>
-		<p>API Redirect URI: <input type="text" name="$uri_field_name" value="$uri_val" size="20"></p>
-		<hr />
-		<p class="submit">
-			<input type="submit" name="Submit" class="button-primary" value="Save Changes" />
-		</p>
+		<form name="linkedin-api-form" method="post">
+			<p>API Key: <input type="text" name="$key_field_name" value="$key_val" size="20"></p>
+			<p>API Secret: <input type="text" name="$secret_field_name" value="$secret_val" size="20"></p>
+			<p>API Redirect URI: <input type="text" name="$uri_field_name" value="$uri_val" size="20"></p>
+			<hr />
+			<p class="submit">
+				<input type="hidden" name="$hidden_field_name" value="Y">
+				<input type="submit" name="Submit" class="button-primary" value="Save Changes" />
+			</p>
 		</form>
 		</div>
 HEREDOC;
@@ -89,72 +83,74 @@ HEREDOC;
  *  Notices      								     *
  *****************************************************/
 
-if(isset($_GET['page']) && $_GET['page'] != "linkedin-groups-block" && (!get_option( 'sufi_linkedin_opts' )["key"] || !get_option( 'sufi_linkedin_opts' )["secret"] || !get_option( 'sufi_linkedin_opts' )["uri"] )){
-	add_action( 'admin_notices', 'linkedin_api_admin_notices' );
+if ( isset( $_GET['page'] ) && $_GET['page'] != "linkedin-groups-block" &&
+	( !get_option( 'sufi_linkedin_opts' )["key"] || !get_option( 'sufi_linkedin_opts' )["secret"] || !get_option( 'sufi_linkedin_opts' )["uri"] ) ) {
+	add_action( 'admin_notices' , 'linkedin_api_admin_notices' );
 }
 
 function remove_notice() {
-	remove_action( 'admin_notices', 'linkedin_api_admin_notices' );
+	remove_action( 'admin_notices' , 'linkedin_api_admin_notices' );
 }
 
 function linkedin_api_admin_notices() {
 	echo "<div class='updated fade error'><p>LinkedIn Groups Plugin is not configured yet. <a href='".get_admin_url()."options-general.php?page=linkedin-groups-block'>Please do it now</a>.</p></div>\n";
 }
-function getRevokeLink(){
-	echo '<a href="'.add_query_arg('LinkedIn','revoke', get_the_permalink()).'" title="Revoke LinkedIn Authorization">Revoke</a><br />';
+function getLogoutLink() {
+	echo '<a href="'.add_query_arg( 'LinkedIn' , 'revoke' , get_the_permalink() ).'" title="Log out of LinkedIn">Log out of LinkedIn</a><br />';
 }
-function getAuthorizationLink(){
-	echo '<a href="'.getAuthorizationCode().'" title="Authorize at LinkedIn">Authorize</a>';
+function getAuthorizationLink() {
+	echo 'You must <a href="'.getAuthorizationCode().'" title="Log in to LinkedIn">Login with LinkedIn</a> to view entire content of this page!';
 }
 /*****************************************************
  *  Shortcodes     								     *
  *****************************************************/
-function linkedin_shortcode( $atts , $content=null) {
-	// OAuth 2 Control Flow
-	if (isset($_GET['error'])) {
-	    // LinkedIn returned an error
-	    print $_GET['error'] . ': ' . $_GET['error_description'];
-	} elseif (isset($_GET['code'])) {
-	    // User authorized your application
-	    if ($_SESSION['state'] == $_GET['state']) {
-	        // Get token so you can make API calls
-	        // echo $_SESSION['access_token'];
-	        getAccessToken();
-	    } else {
-	        // CSRF attack? Or did you mix up your states?
-	        exit;
-	    }
-	} else if( isset( $_GET['LinkedIn'] ) && $_GET['LinkedIn'] == "revoke" ) {
-        $_SESSION = array();
-        getAuthorizationLink();
-	} else { 
-	    if ((empty($_SESSION['expires_at'])) || (time() > $_SESSION['expires_at'])) {
-	        // Token has expired, clear the state
-	         $_SESSION = array();
-	    }
-	    if (!isset($_SESSION['access_token']) || empty($_SESSION['access_token'])) {
-	        getAuthorizationLink();
-	    } 
+function linkedin_shortcode( $atts , $content=null ) {
+	if ( isset( $_GET['error'] ) ) {
+		print $_GET['error'] . ': ' . $_GET['error_description'];
+	} elseif ( isset( $_GET['code'] ) ) {
+		if ( $_SESSION['state'] == $_GET['state'] ) {
+			getAccessToken();
+		} else {
+			// Possible CSRF Attack
+			exit;
+		}
+	} else if ( isset( $_GET['LinkedIn'] ) && $_GET['LinkedIn'] == "revoke" ) {
+			$_SESSION = array();
+			getAuthorizationLink();
+		} else {
+		// Check Token expiry
+		if ( ( empty( $_SESSION['expires_at'] ) ) || ( time() > $_SESSION['expires_at'] ) ) {
+			$_SESSION = array();
+		}
+		// Show authorization link
+		if ( !isset( $_SESSION['access_token'] ) || empty( $_SESSION['access_token'] ) ) {
+			getAuthorizationLink();
+		}
 	}
-    $a = shortcode_atts( array(
-        'group' => ''
-    ), $atts );
+	$a = shortcode_atts( array(
+			'group' => ''
+		), $atts );
 
-    // Congratulations! You have a valid token. Now fetch your profile 
-    if(isset($_SESSION['access_token']) && !empty($_SESSION['access_token'])){
-		$user = fetch('GET', '/v1/people/~/group-memberships/'.$a['group']);
-		// echo '<pre>';
-		// var_dump($user);
-		// echo '</pre>';
-		if(isset($user) && $user->membershipState->code == "member") {
-			getRevokeLink();
+	if ( isset( $_SESSION['access_token'] ) && !empty( $_SESSION['access_token'] ) ) {
+		$group = fetch( 'GET', '/v1/people/~/group-memberships/'.$a['group'] );
+		if ( isset( $group ) && $group->membershipState->code == "member" ) {
+			getLogoutLink();
 			return $content;
 		} else {
-			echo "Not a member of Graduate Business Forum (Alumni Group)";
+			echo '<span style="display: block;
+					border: 1px solid #EDEF00;
+					background: #FFC;
+					color: #777;
+					color: black;
+					border-radius: 5px;
+					padding: 20px 32px 20px 20px;
+					margin: 5px auto;
+					word-wrap: break-word;
+					color: #333;
+					">Sorry, but you must be a member of the <a href="http://www.linkedin.com/groups?gid=1014" title="Graduate Business Forum on LinkedIn">Graduate Business Forum</a> to view this page.</span>';
 		}
-	} 
-    return "";
+	}
+	return "";
 }
 add_shortcode( 'linkedin-group', 'linkedin_shortcode' );
-
 ?>
